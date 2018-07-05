7479cb067481447e705ca6807012bdf792bd4c5f
rashichauhan7 <rashi.chauhan95@gmail.com>
2018-07-05 13:51:29 -0400

    renamed admin folder, added admin page, css and js

--- src/main/java/com/example/myapp/services/UserService.java
+++ src/main/java/com/example/myapp/services/UserService.java
@@ -1,5 +1,22 @@
 package com.example.myapp.services;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.myapp.model.User;
+import com.example.myapp.repository.UserRepository;
+
+@RestController
+
+public class UserService {
+	@Autowired
+	UserRepository repository;
+	@GetMapping("/api/user")
+ public List<User> findAllUsers()
+ {
+	 return (List<User>) repository.findAll();
+ }
 }
---
+++ src/main/webapp/jquery/components/admin/user-admin.controller.client.js
@@ -0,0 +1,36 @@
+//IIFE
+(function (){
+	    var usernameFld, $passwordFld;
+	    var $removeBtn, $editBtn, $createBtn, $searchBtn, $updateBtn;
+	    var $firstNameFld, $lastNameFld;
+	    var $roleFld, $tbody;
+	    //var userService = new AdminUserServiceClient();
+	$(main);	
+	function main()
+	{
+		
+		
+		$removeBtn = $('#remove');
+		$editBtn = $('#edit');
+		$createBtn = $('#create');
+		$searchBtn = $('#search');
+		$updateBtn = $('#update');
+	
+		$createBtn.click(createUser);
+	}
+	    function createUser() {
+	    	$usernameFld = $('#usernameFld').val();
+			$passwordFld = $('#passwordFld').val();
+			$firstNameFld = $('#firstNameFld').val();
+			$lastNameFld = $('#lastNameFld').val();
+			$roleFld = $('#roleFld').val();
+			
+	    	var user;
+	    }
+	    function findAllUsers() {
+	    	
+	    }
+	    function findUserById() { 
+	    	
+	    }
+})();
