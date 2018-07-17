4e38e66ddfb3a21be8d4cf33f6e1c7620f19be46
rashichauhan7 <rashi.chauhan95@gmail.com>
2018-07-17 13:31:14 -0400

    added lesson delete handler

--- src/main/java/com/example/myapp/services/LessonServices.java
+++ src/main/java/com/example/myapp/services/LessonServices.java
@@ -1,10 +1,12 @@
 package com.example.myapp.services;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.myapp.model.Course;
 import com.example.myapp.model.Lesson;
 import com.example.myapp.model.Module;
+import com.example.myapp.model.User;
 import com.example.myapp.repository.CourseRepository;
 import com.example.myapp.repository.LessonRepository;
 import com.example.myapp.repository.ModuleRepository;
@@ -45,15 +48,24 @@ public class LessonServices {
 	        return null;
 	    }
 	    @GetMapping("/api/course/{courseId}/module/{moduleId}/lesson")
-	    public List<Module> findAllLessonsForModule(
+	    public List<Lesson> findAllLessonsForModule(
 	            @PathVariable("courseId") int courseId,@PathVariable("moduleId") int moduleId) {
 	       Optional<Module> mod = moduleRepository.findById(moduleId);
 	        if(mod.isPresent()) {
 	            Module module = mod.get();
-	            module.getLessons();
+	            
+	            return module.getLessons();
 	        }
-	        return null;        
+	        List<Lesson> e = new ArrayList<Lesson>();
+	        return e;        
 	    }
+	    
+	    @DeleteMapping("/api/lesson/{lessonId}")
+		public void deleteLesson(@PathVariable("lessonId") int lessonId)
+		{
+			lessonRepository.deleteById(lessonId);
+		}
+
 }
 
 
